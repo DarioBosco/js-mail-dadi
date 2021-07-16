e9c8806c2663916c53bb6af0d67a6aa19b47f379
Dario Bosco <dario_bosco@protonmail.com>
2021-07-16 14:24:40 +0200

    Esercizio completato

--- js-dadi/js/main.js
+++ js-dadi/js/main.js
@@ -4,3 +4,22 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
+
+//Generare un numero random da 1 a 6
+
+var userNumber = Math.floor(Math.random() * 6 + 1);
+var computerNumber = Math.floor(Math.random() * 6 + 1);
+
+if (userNumber > computerNumber) {
+	console.log('Utente Vince!');
+	console.log('Utente: ', userNumber);
+	console.log('Computer: ' + computerNumber);
+} else if (userNumber < computerNumber) {
+	console.log('Computer Vince!');
+	console.log('Utente: ', userNumber);
+	console.log('Computer: ' + computerNumber);
+} else {
+	console.log('Pareggio!');
+	console.log('Utente: ', userNumber);
+	console.log('Computer: ' + computerNumber);
+}
