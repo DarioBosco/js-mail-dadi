579c68412c23e3b771f6dcccb790e74c3372f89c
Dario Bosco <dario_bosco@protonmail.com>
2021-07-16 17:31:35 +0200

    Esercizio completato

--- js-mail/js/main.js
+++ js-mail/js/main.js
@@ -1,3 +1,4 @@
+/*
 var emailsArray = ['info@example.com', 'admin@example.com', 'contact@example.com'];
 
 var inputEmail = prompt('Inserisci la tua email');
@@ -6,4 +7,22 @@ if (emailsArray.indexOf(inputEmail) == '-1') {
 	console.log('Email non trovata');
 } else {
 	console.log('Email trovata');
+}
+
+*/
+
+var emailsArray = ['info@example.com', 'admin@example.com', 'contact@example.com'];
+var inputEmail = prompt('Inserisci la tua email');
+var foundEmail;
+
+for (let i = 0; i < emailsArray.length; i++) {
+	var element = emailsArray[i];
+
+	if (inputEmail == element) {
+		foundEmail = element;
+		console.log('Email trovata');
+	}
+}
+if (foundEmail == undefined) {
+	console.log('Email non trovata');
 }
